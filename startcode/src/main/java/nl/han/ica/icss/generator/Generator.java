acaceeb7868db3f42794ab4d3dee206efd508aa2
Oscar van der Velde <oyd.vandervelde@student.han.nl>
2025-10-23 19:13:54 +0200

    Generator gemaakt, geeft nu een css2 compliant string terug

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,15 +1,79 @@
 package nl.han.ica.icss.generator;
 
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
 
-import nl.han.ica.icss.ast.AST;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Generator {
 
 	public String generate(AST ast) {
-        return "";
-
-
+		StringBuilder stringBuilder = new StringBuilder();
+		generateNodes(ast.root.body, stringBuilder, 0);
+		return stringBuilder.toString();
 	}
 
-	
+	private void generateNodes(Iterable<ASTNode> nodes, StringBuilder stringBuilder, int indentLevel) {
+		for (ASTNode node : nodes) {
+			if (node instanceof Stylerule) {
+				generateStylerule((Stylerule) node, stringBuilder, indentLevel);
+			} else if (node instanceof Declaration) {
+				generateDeclaration((Declaration) node, stringBuilder, indentLevel);
+			}
+		}
+	}
+
+	private void zgenerateStylerule(Stylerule rule, StringBuilder stringBuilder, int indentLevel) {
+		String indent = "  ".repeat(indentLevel);
+
+		String selectorString = rule.selectors.stream()
+				.map(Object::toString)
+				.reduce((a, b) -> a + ", " + b)
+				.orElse("");
+		stringBuilder.append(indent).append(selectorString).append(" {\n");
+
+		Map<String, Declaration> finalDeclarations = new LinkedHashMap<>();
+		for (ASTNode node : rule.body) {
+			if (node instanceof Declaration) {
+				finalDeclarations.put(((Declaration) node).property.name, (Declaration) node);
+			}
+		}
+
+		for (Declaration declaration : finalDeclarations.values()) {
+			generateDeclaration(declaration, stringBuilder, indentLevel + 1);
+		}
+
+		for (ASTNode node : rule.body) {
+			if (node instanceof Stylerule) {
+				generateStylerule((Stylerule) node, stringBuilder, indentLevel + 1);
+			}
+		}
+
+		stringBuilder.append(indent).append("}\n\n");
+	}
+
+	private void generateDeclaration(Declaration declaration, StringBuilder stringBuilder, int indentLevel) {
+		String indent = "  ".repeat(indentLevel);
+		stringBuilder.append(indent)
+				.append(declaration.property.name)
+				.append(": ")
+				.append(literalToString(declaration.expression))
+				.append(";\n");
+	}
+
+	private String literalToString(Expression expression) {
+		if (expression instanceof PixelLiteral) {
+			return ((PixelLiteral) expression).value + "px";
+		} else if (expression instanceof PercentageLiteral) {
+			return ((PercentageLiteral) expression).value + "%";
+		} else if (expression instanceof ScalarLiteral) {
+			return String.valueOf(((ScalarLiteral) expression).value);
+		} else if (expression instanceof BoolLiteral) {
+			return String.valueOf(((BoolLiteral) expression).value);
+		} else if (expression instanceof ColorLiteral) {
+			return ((ColorLiteral) expression).value;
+		}
+		return "";
+	}
 }
